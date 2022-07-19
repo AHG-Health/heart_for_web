fdf7b3326c1aed805c61e1762815e06397c0b6f1
Elbie de Swardt <78783050+elbie-deswardt@users.noreply.github.com>
2022-07-19 08:32:32 +0200

    Changed correlation parameters
    Change correlation parameters to Mr. Nieuwoudt's spesification.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,9 +38,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5b92465eed0fe2b9888b6589408153cb",
-"/": "5b92465eed0fe2b9888b6589408153cb",
-"main.dart.js": "e14c5e8232d77181b2c10bfbd5f85d34",
+"index.html": "fb5c8cc38596bd27967c620b71f8f889",
+"/": "fb5c8cc38596bd27967c620b71f8f889",
+"main.dart.js": "cdcd92e9a2297b9696d800106bf90e20",
 "manifest.json": "ae0ece01676d410a033f9cc7cfa4e0c3",
 "version.json": "fd3333857a44e8c40bc8301dce9b6947"
 };
