02b02b4026f36c3692c3fc0fb5707e503572f7d9
Elbie de Swardt <78783050+elbie-deswardt@users.noreply.github.com>
2022-10-03 10:34:29 +0200

    Feature Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,8 +38,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ca8e77990ae64ec2e255ff08e22acef6",
-"/": "ca8e77990ae64ec2e255ff08e22acef6",
+"index.html": "741a115c356c271d2a7a33dcba38d1ac",
+"/": "741a115c356c271d2a7a33dcba38d1ac",
 "main.dart.js": "aedc95b950ca219c90c6c02f0bce8d99",
 "manifest.json": "9c3cda0e499cd886067a652374e21af6",
 "version.json": "fd3333857a44e8c40bc8301dce9b6947"
